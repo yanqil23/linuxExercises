3bbdecdfc0242d738b24c15725e20339481864a7
RUI ZHANG <rzhang534@wisc.edu>
2024-10-15 13:44:33 -0500

    Added five_dirs.sh to create directories and files with corresponding content

--- five_dirs.sh
+++ five_dirs.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+mkdir -p five
+
+for dir_num in {1..5}; do
+    mkdir -p five/dir$dir_num
+
+    for file_num in {1..4}; do
+        seq -s '\n' $file_num > five/dir$dir_num/file$file_num
+    done
+done
+
