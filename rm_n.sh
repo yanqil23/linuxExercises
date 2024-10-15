7634b966680e6443c9abe0a7a7047444aa022a18
Shumeng <sfang58@wisc.edu>
2024-10-15 14:51:19 -0500

    Update rm_n.sh to remove files over n byte

--- rm_n.sh
+++ rm_n.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+# usage statement
+usage() {
+    echo "usage: $0 <dir> <n>" 1>&2
+    exit 1
+}
+dir="$1"
+
+n="$2"
+
+find "$dir" -type f -size +"$n"c -exec rm -f {} \;
+
+echo "Removed files larger than $n bytes in directory $dir."
