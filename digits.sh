ab4a3bd85fc63d6ff2e96f79feb42aa44227f69a
RUI ZHANG <rzhang534@wisc.edu>
2024-10-15 13:37:20 -0500

    Added digits.sh to find sum of numbers between 1000 and 2000 with only digits 0 and 1

--- digits.sh
+++ digits.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+# Initialize sum variable
+sum=0
+
+# Loop over numbers from 1000 to 2000
+for num in {1000..2000}; do
+    # Check if the number contains only digits 0 and 1 using a regex
+    if [[ $num =~ ^[01]+$ ]]; then
+        # Add the number to the sum
+        sum=$((sum + num))
+    fi
+done
+
+# Print the result
+echo "The sum of numbers between 1000 and 2000 with only digits 0 and 1 is: $sum"
+
