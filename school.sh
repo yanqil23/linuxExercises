3d2d5d23e8fdf76dd3a979dd84403ab166aae0ac
Yanqi Liu <liu2496@wisc.edu>
2024-10-17 13:57:03 -0500

    revised school.sh

--- school.sh
+++ school.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-total_value=$(grep "MADISON SCHOOLS" Property_Tax_Roll.csv | cut -d ',' -f 7)
+cat Property_Tax_Roll.csv |
+grep "MADISON SCHOOLS" |
+cut -d ',' -f 7 |
+{
+    sum=0
+    count=0
 
-sum=0
-count=0
+    while read -r value; do
+        sum=$(echo "$sum + $value" | bc)
+        count=$((count + 1))
+    done
 
-for value in $total_value; do
-    sum=$(echo "$sum + $value" | bc)
-    count=$((count + 1))
-done
-
-if [ $count -ne 0 ]; then
-    average=$(echo "scale=2; $sum / $count" | bc)
-    echo "Average TotalAssessedValue: $average"
-else
-    echo "No data found"
-fi
-
+    if [ $count -ne 0 ]; then
+        average=$(echo "scale=2; $sum / $count" | bc)
+        echo "Average TotalAssessedValue: $average"
+    else
+        echo "No data found"
+    fi
+}
